27f61e617d8f6e97246094f67f0cd933e390babb
Kayan de Souza <40444457+KayandeSouzaPereira@users.noreply.github.com>
2023-12-16 16:35:06 -0300

    publicação
    publicação

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import {
   AboutSection,
-  ArticlesSection,
   ContactSection,
   HeroSection,
   InterestsSection,
@@ -13,14 +12,12 @@ import {
 export default function IndexPage() {
   return (
     <>
-      <Seo title="Gatsby Starter for Portfolio Minimal" />
+      <Seo title="Portfolio" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
-        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
-        <AboutSection sectionId="about" heading="About Portfolio Minimal" />
-        <InterestsSection sectionId="details" heading="Details" />
-        <ProjectsSection sectionId="features" heading="Built-in Features" />
-        <ContactSection sectionId="github" heading="Issues?" />
+        <AboutSection sectionId="sobre" heading="About Portfolio Minimal" />
+        <ProjectsSection sectionId="projetos" heading="Built-in Features" />
+        <ContactSection sectionId="contato" heading="Issues?" />
       </Page>
     </>
   );
