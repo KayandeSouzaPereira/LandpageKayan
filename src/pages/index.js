0bb5447ca5c2823f3ce35850e872ff60dfac6dda
Kayan de Souza <40444457+KayandeSouzaPereira@users.noreply.github.com>
2024-12-05 19:18:54 -0300

    Adição de detalhes de projeto
    Adição de detalhes de projeto

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  HeroSection,
   AboutSection,
   ContactSection,
   Page,
@@ -13,6 +14,9 @@ export default function IndexPage() {
       <Seo title="Kayan Pereira" />
       <Page useSplashScreenAnimation>
         <iframe src='https://my.spline.design/particlenebula-86230cc87752e77a51907b3f39fd4a15/' frameborder='0' width='100%' height='30%'/>
+        <div style={{display:"flex",position:"absolute",top:200,left: 50}}><HeroSection sectionId="hero"/></div>
+       {/*  <div style={{display:"flex",position:"absolute",top:200,right: 50,backgroundColor:"#000000",width:450,height:500}}></div> */}
+        <div style={{height:50}}></div>
         <AboutSection sectionId="sobre" heading="Sobre mim" />
         <ProjectsSection sectionId="projetos" heading="Projetos" />
         <ContactSection sectionId="contato" heading="Contato" />
