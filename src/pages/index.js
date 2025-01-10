b379e61d9b5ebfbfa826925d25654c2ddadb4198
Kayan de Souza <40444457+KayandeSouzaPereira@users.noreply.github.com>
2025-01-10 17:32:45 -0300

    update formatação do site
    update formatação do site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react"
 import {
   HeroSection,
   AboutSection,
@@ -9,14 +9,38 @@ import {
 } from "gatsby-theme-portfolio-minimal";
 
 export default function IndexPage() {
+
+  const [isDesktop, setDesktop] = useState(false)
+
+  useEffect(()=>{
+    if(typeof window !== 'undefined'){
+      setDesktop(window.innerWidth > 768)
+    }
+  },[])
+
+  const updateMedia = () => {
+    setDesktop(window.innerWidth > 768)
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia)
+    return () => window.removeEventListener("resize", updateMedia)
+  })
+
+
   return (
     <>
       <Seo title="Kayan Pereira" />
       <Page useSplashScreenAnimation>
+        
+        {isDesktop ? (
+          <>
         <iframe src='https://my.spline.design/particlenebula-86230cc87752e77a51907b3f39fd4a15/' frameborder='0' width='100%' height='30%'/>
-        <div style={{display:"flex",position:"absolute",top:200,left: 50}}><HeroSection sectionId="hero"/></div>
+        <div style={{display:"flex",position:"absolute",top:200,left: 50}}><HeroSection sectionId="hero"/></div><div style={{height:50}}></div></>):(
+          <><iframe src='https://my.spline.design/particlenebula-86230cc87752e77a51907b3f39fd4a15/' frameborder='0' width='100%' height='20%'/><div style={{display:"flex",marginLeft:10}}><HeroSection sectionId="hero"/></div></>
+        )}
        {/*  <div style={{display:"flex",position:"absolute",top:200,right: 50,backgroundColor:"#000000",width:450,height:500}}></div> */}
-        <div style={{height:50}}></div>
+        
         <AboutSection sectionId="sobre" heading="Sobre mim" />
         <ProjectsSection sectionId="projetos" heading="Projetos" />
         <ContactSection sectionId="contato" heading="Contato" />
